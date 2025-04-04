2bdc54a618395a523007ca1b4509ed463bdcb543
Jmarser <juanmarser@signlab.info>
2025-04-04 21:53:28 +0200

    Implemenado hilt básico

---
+++ app/src/main/java/com/jmarser/mydelivery/MyDelivery.kt
@@ -0,0 +1,15 @@
+package com.jmarser.mydelivery
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Project: My Delivery
+ * File: MyDelivery
+ * Author: Tu Jmarser <aenur32@gmail.com>
+ * Created: 04/04/2025
+ */
+
+@HiltAndroidApp
+class MyDelivery: Application() {
+}
